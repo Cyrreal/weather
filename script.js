046236bba903f0830b6a8ab0c835837a788dbd80
Cyrreal <lazukinki@gmail.com>
2022-05-01 19:27:03 +0300

    описаня

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const link =
-  "http://api.weatherstack.com/current?access_key=f2976b78f737b2ff67b8e1b7ac7f8092";
+  "http://api.weatherstack.com/current?access_key=f2976b78f737b2ff67b8e1b7ac7f8092"; /// создаю переменную для ссылки
 
 const main = document.querySelector(".all-info");
 const change = document.querySelector(".btn-change");
@@ -8,34 +8,37 @@ const allInfo = document.querySelector(".all-info");
 const popup = document.getElementById("popup");
 const findButton = document.querySelector(".find-city");
 const cityInput = document.querySelector(".city-input");
-const form = document.querySelector(".form");
+const form = document.querySelector(".form"); //// получаю все нужные мне узлы
 
-let store = {
+let object = {
   city: "Moscow",
   description: 0,
   temperature: 0,
-};
+}; ////создаю объект с которым буду работать
 
+//// Пишу асинхронную функцию для получения данных с сервера и отрисовки блоков по полученным данным
 const fetchData = async () => {
-  const fetchResult = await fetch(`${link}&query=${store.city}`);
-  const data = await fetchResult.json();
-
+  const fetchResult = await fetch(`${link}&query=${object.city}`);
+  // console.log(fetchResult);
+  const data = await fetchResult.json(); //// получаю объект с сервера
+  console.log(data);
   const {
     current: { city, weather_descriptions: description, temperature },
-  } = data;
+  } = data; ////деструктуризирую полученный объект
 
-  store = {
-    ...store,
+  object = {
+    ...object,
     temperature,
-    description: description[0],
+    description: description[0], ////перезаписываю свой объект, description[0] так как с сервера под этим ключом приходит массив с 1 элементом, обращаюсь к нему
   };
 
-  console.log(store);
-  createDiv();
+  console.log(object);
+  createDiv(); ////отрисовываю объект как приходит промис
 };
 
 const divInfo = () => {
-  const { city, description, temperature } = store;
+  ////отдельная функция для создания нового узла
+  const { city, description, temperature } = object;
   return `
     <div class="weather-info">
         <h1>${temperature}℃</h1>
@@ -46,12 +49,15 @@ const divInfo = () => {
         </div>`;
 };
 
+////Функция для отрисовки, которая вставляет созданный мной объект
 const createDiv = () => {
   main.innerHTML = divInfo();
   const change = document.querySelector(".btn-change");
 
-  change.addEventListener("click", userClick);
+  change.addEventListener("click", userClick); ////добавляю слушатель внутри этой функции, так как до ее вызова, узла не существует, а функция не выполнится пока промис не придет
 };
+
+////все что ниже связанно с добавлением слушателей на кнопочки и переключением классов
 const userClick = () => {
   popup.classList.toggle("active");
 };
@@ -65,13 +71,14 @@ const handleSubmit = (event) => {
 findButton.addEventListener("click", handleSubmit);
 
 const handleInput = (element) => {
-  store = {
-    ...store,
-    city: element.target.value,
+  object = {
+    ...object,
+    city: element.target.value, ////перезаписываю элемент, меняя в нем город на введенный пользователем
   };
 };
 
 cityInput.addEventListener("input", handleInput);
 form.addEventListener("submit", handleSubmit);
 
+////вызываю  свою асинхронную функцию
 fetchData();
